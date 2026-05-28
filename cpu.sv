782dd5d95bc6d8cf049de0c5ee10cc2a84e7c0cc
Ali Tahir <blah4209211@icloud.com>
2026-05-28 22:18:53 +0500

    fix bit width errors and dmem module

--- cpu.sv
+++ cpu.sv
@@ -130,30 +130,29 @@ endmodule
 module dmem (
     input  logic        clk,
     input  logic        mem_wen,
-    input  logic [31:0] raddr,
-    input  logic [31:0] waddr,
-    input  logic [2:0]  rmask,
-    input  logic [2:0]  wmask,
+    input  logic [31:0] addr,
+    input  logic [1:0]  rmask,
+    input  logic [1:0]  wmask,
     input  logic [31:0] mem_wdata,
     output logic [31:0] mem_rdata
 );
-    logic [31:0] memory [1024*1024*1024] = '{default: '0};
+    logic [31:0] memory [1024*1024] = '{default: '0};
     always_ff @(posedge clk) begin
         if (mem_wen) begin
-            memory[waddr] <= mem_wdata;
+            memory[addr] <= mem_wdata;
 			case (wmask)
-				2'd0: memory[waddr] <= mem_wdata;
-				2'd1: memory[waddr] <= {16'd0,mem_wdata[15:0]};
-				2'd2: memory[waddr] <= {24'd0,mem_wdata[7:0]};
+				2'd0: memory[addr] <= mem_wdata;
+				2'd1: memory[addr] <= {16'd0,mem_wdata[15:0]};
+				2'd2: memory[addr] <= {24'd0,mem_wdata[7:0]};
 				default: begin end
 			endcase
         end
     end
 	always @(*) begin
 		case (rmask)
-			2'd0: mem_rdata = memory[raddr];
-			2'd1: mem_rdata = {16'd0,memory[raddr][15:0]};
-			2'd2: mem_rdata = {24'd0,memory[raddr][7:0]};
+			2'd0: mem_rdata = memory[addr];
+			2'd1: mem_rdata = {16'd0,memory[addr][15:0]};
+			2'd2: mem_rdata = {24'd0,memory[addr][7:0]};
 			default: begin end
 		endcase
 	end
@@ -163,8 +162,8 @@ module imem (
     input  logic [31:0] addr,
     output logic [31:0] instr
 );
-    logic [31:0] memory [1024*1024*1024]; // 4GB imem
-    assign instr = memory[addr[31:2]];
+    logic [31:0] memory [1024*1024]; // 1MB imem
+    assign instr = memory[addr[21:2]];
 endmodule
 
 module branch_compare (
@@ -496,8 +495,7 @@ module cpu (
 
 	dmem dmem_inst(
 		.clk(clk), 
-		.raddr(alu_out_2),
-		.waddr(alu_out_2),
+		.addr(alu_out_2),
 		.mem_wen(mem_wen_2), 
 		.rmask(rmask_2),
 		.wmask(wmask_2),
